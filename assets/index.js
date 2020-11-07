3eb3bee484926998f91aee1a73cfdf7975f03f62
nbmc1991 <nbmc1991@gmail.com>
2020-11-06 22:24:49 -0500

    more changes to javascript and html

--- assets/index.js
+++ assets/index.js
@@ -1,5 +1,32 @@
 const navbar =document.querySelector(".navbar");
-const navbarOffsetTop = navbar.navbarOffsetTop;
-window.addEventListener("scroll",() =>{
+const navbarOffsetTop = navbar.offsetTop;
 
+//this section will allow user to change the app light to dark theme
+// const themeSwitcher = document.querySelector("#theme-switcher");
+// const themeSection = document.querySelector(".section-1");
+// const mode ="dark";
+
+// themeSection.addEventListener("click", ()=>{
+//     if (mode ==="dark"){
+//         mode = "light";
+//         themeSection.setAttribute("class","light");
+//     }else{
+//         mode="dark";
+//         themeSection.setAttribute("class","dark");
+//     }
+// });
+
+
+
+
+// confirm("Hello! Welcome to my Portfolio Page !")
+
+window.addEventListener("scroll",function(){
+    // console.log(window.pageYOffset, navbar.offsetTop);
+if (window.pageYOffset >= navbarOffsetTop){
+    navbar.classList.add("sticky")
+}else{navbar.classList.remove("sticky")
+
+}
 });
+

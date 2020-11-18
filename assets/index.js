9b5a04c79d4e9b37b01422153b30a6daf4bf927f
nbmc1991 <nbmc1991@gmail.com>
2020-11-18 14:30:10 -0500

    chaning themes

--- assets/index.js
+++ assets/index.js
@@ -15,6 +15,13 @@
 //     }
 // });
 
+function toggle(){
+    var sec = document.getElementById('sec');
+    var nav = document.getElementById('navigation');
+    sec.classList.toggle('active');
+    nav.classList.toggle('active');
+}
+
 
 
 
